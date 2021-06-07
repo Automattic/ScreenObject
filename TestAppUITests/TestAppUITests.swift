ae86c6be8f759f78055ffa75c1ae933176a4aca6
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-07 20:36:52 +1000

    Add app and UI test target to verify framework integration and behavior

---
+++ TestAppUITests/TestAppUITests.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+class TestAppUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        XCTAssertTrue(app.staticTexts["Hello, world!"].exists)
+    }
+}
