489fa03be68eeccaf705fc938c22340284623d72
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-07 21:01:51 +1000

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "ScreenObject",
+    platforms: [ .iOS(.v13) ],
+    products: [
+        .library(name: "ScreenObject", targets: ["ScreenObject"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "ScreenObject", dependencies: []),
+    ],
+    swiftLanguageVersions: [.v5]
+)
