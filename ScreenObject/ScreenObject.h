06ab3cbed61e77e8c3ee85ed9dbf055c71ab1c3d
Gio Lodi <giovanni.lodi42@gmail.com>
2021-06-07 20:26:38 +1000

    Generate framework target for ScreenObject with Xcode

---
+++ ScreenObject/ScreenObject.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScreenObject.
+FOUNDATION_EXPORT double ScreenObjectVersionNumber;
+
+//! Project version string for ScreenObject.
+FOUNDATION_EXPORT const unsigned char ScreenObjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScreenObject/PublicHeader.h>
