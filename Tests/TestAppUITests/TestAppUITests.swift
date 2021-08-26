ece1e7ef21ff5b2e3a7a279c78e4f91ad543fdc1
Gio Lodi <giovanni.lodi42@gmail.com>
2021-08-26 14:45:57 +1000

    Add test for current `init` behavior

--- Tests/TestAppUITests/TestAppUITests.swift
+++ Tests/TestAppUITests/TestAppUITests.swift
@@ -14,11 +14,28 @@ class TestAppUITests: XCTestCase {
         let screen = try HelloWorldScreen()
         XCTAssertTrue(screen.isLoaded)
     }
+
+    func testScreenInitFailsWhenScreenIsNotLoaded() throws {
+        XCTExpectFailure("Attempting to init a screen that is not loaded currently fails the tests")
+        _ = try MissingScreen(app: app)
+    }
 }
 
 final class HelloWorldScreen: ScreenObject {
 
     init(app: XCUIApplication = XCUIApplication()) throws {
         try super.init(expectedElementGetter: { $0.staticTexts["Hello, world!"] })
+    }
+}
+
+/// A screen that doesn't exist. Use it to test the init failure behavior.
+class MissingScreen: ScreenObject {
+
+    init(app: XCUIApplication) throws {
+        try super.init(
+            expectedElementGetter: { $0.staticTexts["this screen does not exist"] },
+            app: app,
+            waitTimeout: 1
+        )
     }
 }
